2e5dda0d4574c55fc072ddb06bdf2bcf54c7b9a6
mocchira <nekotaroh@gmail.com>
2014-12-16 00:47:19 +0000

    Update detail.hpp to replace eleveldb with erocksdb

--- c_src/detail.hpp
+++ c_src/detail.hpp
@@ -20,24 +20,24 @@
 //
 // -------------------------------------------------------------------
 
-#ifndef __ELEVELDB_DETAIL_HPP
- #define __ELEVELDB_DETAIL_HPP 1
+#ifndef __EROCKSDB_DETAIL_HPP
+ #define __EROCKSDB_DETAIL_HPP 1
 
 #include <stdint.h>
 #include <stddef.h>
 
 /* These can be hopefully-replaced with constexpr or compile-time assert later: */
 #if defined(OS_SOLARIS) || defined(SOLARIS) || defined(sun)
- #define ELEVELDB_IS_SOLARIS 1
+ #define EROCKSDB_IS_SOLARIS 1
 #else
- #undef ELEVELDB_IS_SOLARIS
+ #undef EROCKSDB_IS_SOLARIS
 #endif
 
-#ifdef ELEVELDB_IS_SOLARIS
+#ifdef EROCKSDB_IS_SOLARIS
  #include <atomic.h>
 #endif
 
-namespace eleveldb {
+namespace erocksdb {
 
 // primary template
 template <typename PtrT, typename ValueT>
@@ -48,7 +48,7 @@ inline bool compare_and_swap(volatile PtrT *ptr, const ValueT& comp_val, const V
 template <>
 inline bool compare_and_swap(volatile uint32_t *ptr, const int& comp_val, const int& exchange_val)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
   return ((uint32_t) comp_val==atomic_cas_32(ptr, comp_val, exchange_val));
 #else
     return __sync_bool_compare_and_swap(ptr, comp_val, exchange_val);
@@ -60,7 +60,7 @@ inline bool compare_and_swap(volatile uint32_t *ptr, const int& comp_val, const 
 template <typename PtrT, typename ValueT>
 inline bool compare_and_swap(volatile PtrT *ptr, const ValueT& comp_val, const ValueT& exchange_val)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
     return (comp_val==atomic_cas_ptr(ptr, comp_val, exchange_val));
 #else
     return __sync_bool_compare_and_swap(ptr, comp_val, exchange_val);
@@ -73,7 +73,7 @@ inline ValueT inc_and_fetch(volatile ValueT *ptr);
 template <>
 inline uint64_t inc_and_fetch(volatile uint64_t *ptr)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
     return atomic_inc_64_nv(ptr);
 #else
     return __sync_add_and_fetch(ptr, 1);
@@ -83,7 +83,7 @@ inline uint64_t inc_and_fetch(volatile uint64_t *ptr)
 template <>
 inline uint32_t inc_and_fetch(volatile uint32_t *ptr)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
     return atomic_inc_32_nv(ptr);
 #else
     return __sync_add_and_fetch(ptr, 1);
@@ -104,7 +104,7 @@ inline ValueT dec_and_fetch(volatile ValueT *ptr);
 template <>
 inline uint64_t dec_and_fetch(volatile uint64_t *ptr)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
     return atomic_dec_64_nv(ptr);
 #else
     return __sync_sub_and_fetch(ptr, 1);
@@ -114,7 +114,7 @@ inline uint64_t dec_and_fetch(volatile uint64_t *ptr)
 template <>
 inline uint32_t dec_and_fetch(volatile uint32_t *ptr)
 {
-#if ELEVELDB_IS_SOLARIS
+#if EROCKSDB_IS_SOLARIS
     return atomic_dec_32_nv(ptr);
 #else
     return __sync_sub_and_fetch(ptr, 1);
@@ -129,6 +129,6 @@ inline size_t dec_and_fetch(volatile size_t *ptr)
 }
 #endif
 
-} // namespace eleveldb::detail
+} // namespace erocksdb::detail
 
 #endif

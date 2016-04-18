3adb234f88d77311b84632011a38ff6a30234e81
benoitc <bchesneau@gmail.com>
2016-04-18 19:28:49 +0200

    move snapshots operations in their own module

--- c_src/erocksdb.cc
+++ c_src/erocksdb.cc
@@ -1044,71 +1044,7 @@ ERL_NIF_TERM write_batch_item(ErlNifEnv* env, ERL_NIF_TERM item, rocksdb::WriteB
 namespace erocksdb {
 
 
-ERL_NIF_TERM
-snapshot(
-    ErlNifEnv* env,
-    int argc,
-    const ERL_NIF_TERM argv[])
-{
-    ReferencePtr<DbObject> db_ptr;
-    SnapshotObject* snapshot_ptr;
-    const rocksdb::Snapshot* snapshot;
-    const ERL_NIF_TERM& handle_ref = argv[0];
 
-
-    db_ptr.assign(DbObject::RetrieveDbObject(env, handle_ref));
-
-    if(NULL==db_ptr.get())
-    {
-        return enif_make_badarg(env);
-    }
-
-    // is this even possible?
-    if(NULL == db_ptr->m_Db)
-    {
-        return error_einval(env);
-    }
-
-    // create snapshot
-    snapshot = db_ptr->m_Db->GetSnapshot();
-    snapshot_ptr = SnapshotObject::CreateSnapshotObject(db_ptr.get(), snapshot);
-
-    // create a resource reference to send erlang
-    ERL_NIF_TERM result = enif_make_resource(env, snapshot_ptr);
-    // clear the automatic reference from enif_alloc_resource in SnapshotObject
-    enif_release_resource(snapshot_ptr);
-    snapshot = NULL;
-
-    return enif_make_tuple2(env, ATOM_OK, result);
-}   // snapshot
-
-
-ERL_NIF_TERM
-release_snapshot(
-    ErlNifEnv* env,
-    int argc,
-    const ERL_NIF_TERM argv[])
-{
-
-    const ERL_NIF_TERM& handle_ref = argv[0];
-
-    ReferencePtr<SnapshotObject> snapshot_ptr;
-    snapshot_ptr.assign(SnapshotObject::RetrieveSnapshotObject(env, handle_ref));
-
-    if(NULL==snapshot_ptr.get())
-    {
-        return ATOM_OK;
-    }
-
-    // release snapshot object
-    SnapshotObject* snapshot = snapshot_ptr.get();
-    snapshot->m_DbPtr->m_Db->ReleaseSnapshot(snapshot->m_Snapshot);
-
-    // set closing flag
-    ErlRefObject::InitiateCloseRequest(snapshot);
-
-    return ATOM_OK;
-}   // release_snapshot
 
 
 ERL_NIF_TERM
